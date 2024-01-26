2b47470ef53a789b70d47c811c6b020ac9906996
JD-janydaniel <janydaniel59@gmail.com>
2024-01-26 12:53:18 +0530

    todays change

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/#endpoints-all")
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    for(var i=0;i<res.length;i++){
+    console.log(res[i].name.common)
+    }
+    for(var j=0;j<res.length;j++){
+        if(res[j].capital){
+        console.log(res[j].capital[0]+" "+res[j].area)
+        }
+        else{
+            console.log("invalid capital"+res[j].area)
+        }
+    }
+}
