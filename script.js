fcdf719f707c1eb2c3839bc4e20f6720e7848a85
JD-janydaniel <janydaniel59@gmail.com>
2024-02-05 12:09:56 +0530

    05-02-2024update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all")
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
 request.send();
 request.onload = function(){
     var res = JSON.parse(request.response);
     console.log(res);
-    for(var i=0;i<res.length;i++){
+   /* for(var i=0;i<res.length;i++){
     console.log(res[i].name.common)
     }
     for(var j=0;j<res.length;j++){
@@ -14,5 +14,24 @@ request.onload = function(){
         else{
             console.log("invalid capital"+res[j].area)
         }
-    }
+    }*/
+    // 1.print only those countries name who belongs to asia region
+// this will show whole detail of the countries
+//var result_asia = res.filter((ele)=>ele.region == "Asia");
+//console.log(result_asia);
+// this will show only the countries name
+//var country_names = result_asia.map((ele)=> ele.name);
+//console.log(country_names);
+
+// 2.print capital of those countries whosw population is less thsn 2lakhs
+ //var popul = res.filter((ele) => ele.population <= 200000);
+// console.log(popul);
+ //this will show the capital
+ //var Capital = popul.map((ele)=> ele.capital);
+ //console.log(Capital);
+
+var total_poul = res.reduce((acc,cv)=>acc+cv.population,0);
+console.log(total_poul);
+
+
 }
